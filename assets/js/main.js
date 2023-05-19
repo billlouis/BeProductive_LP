bd652f46320de277134ba086e19a5c1b20013ae5
Bill <billlouis.bl@gmail.com>
2023-05-20 02:26:28 +0800

    added animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,3 +88,20 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=>observer.observe(el));
+const hiddenElements2 = document.querySelectorAll('.hidden2');
+hiddenElements2.forEach((el)=>observer.observe(el));
